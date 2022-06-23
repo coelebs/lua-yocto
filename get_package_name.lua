e5c525840ff5d411e78920bd4e56086144146c06
Vincent Kriek <vincent@kriek.org>
2022-06-23 22:39:47 +0200

    Documentation

--- get_package_name.lua
+++ get_package_name.lua
@@ -2,13 +2,15 @@ M = {}
 
 M.index = {}
 
+-- Parse path to return the package name
+-- @param filename: path to the bitbake file
+-- TODO return the package version as well in the object
 local function get_package(filename)
     if not (string.match(filename, "bb$") or string.match(filename, "bbappend$")) then
         return nil
     end
 
     local basename = string.gsub(filename, "(.*/)(.*)", "%2")
-    -- Convention is PACKAGENAME_PACKAGEVERSION.bb{append}
     local index = string.find(basename, "_")
     if index == nil then
         local dot   = string.find(basename, "%.") - 1
@@ -18,6 +20,9 @@ local function get_package(filename)
     end
 end
 
+-- Iterate over every file in the layer directory with a bitbake extension and parse the package
+-- @param layer: layer directory
+-- TODO add layer priority and position in the package object or index table
 local function index_layer(layer)
     local f = io.popen("find " .. layer .. " -iname \"*.bb\"")
     if f ~= nil then
@@ -28,6 +33,9 @@ local function index_layer(layer)
     end
 end
 
+-- Parse layer path from bb layer line
+-- @param line: bb layer line
+-- TODO return the layer priority
 local function get_layer_path(line)
     for w in line:gmatch("%S+") do
         if string.match(w, "/") then
@@ -38,6 +46,7 @@ local function get_layer_path(line)
     return nil
 end
 
+-- Get the layers from bitbake-layers and parse them
 local function get_layers()
     local f = io.popen("bitbake-layers show-layers")
     if f ~= nil then
