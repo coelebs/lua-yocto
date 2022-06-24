8dc7a669f35a6ff544f989c43ad825c00e96535c
Vincent Kriek <vincent@kriek.org>
2022-06-24 22:12:24 +0200

    Parse files into dictonary

--- get_package_name.lua
+++ get_package_name.lua
@@ -4,63 +4,77 @@ M.index = {}
 
 -- Parse path to return the package name
 -- @param filename: path to the bitbake file
--- TODO return the package version as well in the object
-local function get_package(filename)
+-- @param layer: the bitbake layer info
+local function index_recipe(filename, layer)
     if not (string.match(filename, "bb$") or string.match(filename, "bbappend$")) then
         return nil
     end
 
     local basename = string.gsub(filename, "(.*/)(.*)", "%2")
     local index = string.find(basename, "_")
+    local name
     if index == nil then
         local dot   = string.find(basename, "%.") - 1
-        return {string.sub(basename, 0, dot), filename}
+        name = string.sub(basename, 0, dot)
     else
-        return {string.sub(basename, 0, index - 1), filename}
+        name = string.sub(basename, 0, index - 1)
     end
+
+    local recipe = {}
+    recipe["filename"] = filename
+    recipe["layer"] = layer
+
+    if M.index[name] == nil then
+        M.index[name] = {}
+    end
+
+    table.insert(M.index[name], recipe)
 end
 
 -- Iterate over every file in the layer directory with a bitbake extension and parse the package
 -- @param layer: layer directory
--- TODO add layer priority and position in the package object or index table
 local function index_layer(layer)
-    local f = io.popen("find " .. layer .. " -iname \"*.bb\"")
+    local f = io.popen("find " .. layer["path"] .. " -iname \"*.bb\"")
     if f ~= nil then
         local output = f:read("*a")
         for line in output:gmatch("[^\n]+") do
-            M.index[#M.index+1] = get_package(line)
+            index_recipe(line, layer)
         end
     end
 end
 
 -- Parse layer path from bb layer line
 -- @param line: bb layer line
--- TODO return the layer priority
-local function get_layer_path(line)
-    for w in line:gmatch("%S+") do
-        if string.match(w, "/") then
-            return w
-        end
-    end
+-- @param index: bb layer index
+local function get_layer(line, index)
+    local layer = {}
+    layer["index"] = index
 
-    return nil
+    local el = line:gmatch("%S+")
+    layer["name"] = el()
+    layer["path"] = el()
+    layer["priority"] = el()
+
+    return layer
 end
 
 -- Get the layers from bitbake-layers and parse them
 local function get_layers()
     local f = io.popen("bitbake-layers show-layers")
-    if f ~= nil then
-        local output = f:read("*a")
-        for line in output:gmatch("[^\n]+") do
-            if string.match(line, "/") then
-                local layer = get_layer_path(line)
-                if layer ~= nil then
-                    index_layer(layer)
-                end
+    if f == nil then
+        return nil
+    end
+
+    local output = f:read("*a")
+    for line in output:gmatch("[^\n]+") do
+        if string.match(line, "/") then
+            local layer = get_layer(line)
+            if layer ~= nil then
+                index_layer(layer)
             end
         end
-        return f:close()
     end
+    return f:close()
 end
 
 if get_layers() then
