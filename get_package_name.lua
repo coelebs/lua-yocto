35e63425c6a030acc8e32a47c1bb72f100957e39
Vincent Kriek <vincent@kriek.org>
2022-06-26 23:21:02 +0200

    Refector the add recipe method

--- get_package_name.lua
+++ get_package_name.lua
@@ -2,19 +2,8 @@ M = {}
 
 M.index = {}
 
--- Parse path to return the package name
--- @param filename: path to the bitbake file
--- @param layer: the bitbake layer info
-local function index_recipe(filename, layer)
-    local recipetype
-    if string.match(filename, "bb$") then
-        recipetype = "recipe"
-    elseif string.match(filename, "bbappend$") then
-        recipetype = "append"
-    else
-        return nil
-    end
-
+-- Parse bitbake filename
+local function parse_filename(filename)
     local basename = string.gsub(filename, "(.*/)(.*)", "%2")
     local index = string.find(basename, "_")
     local dot   = string.find(basename, ".bb") - 1
@@ -30,18 +19,12 @@ local function index_recipe(filename, layer)
         end
     end
 
-    local recipe = {}
-    recipe.filename = filename
-    recipe.layer = layer
+    return name, version
+end
 
-    if M.index[name] == nil then
-        M.index[name] = {}
-    end
-
-    if version == nil and recipetype == "recipe" then
-        version = "unversioned"
-    end
-
+-- Add recipe to index. When it's an bbappend recipe and unversioned, it will
+-- be added to all the available recipe versions
+local function add_recipe(name, version, recipetype, recipe)
     if version == nil and recipetype == "append" then
         for _, v in pairs(M.index[name]) do
             table.insert(v, recipe)
@@ -55,6 +38,36 @@ local function index_recipe(filename, layer)
 
 
     table.insert(M.index[name][version], recipe)
+end
+
+-- Parse path to return the package name
+-- @param filename: path to the bitbake file
+-- @param layer: the bitbake layer info
+local function index_recipe(filename, layer)
+    local recipetype
+    if string.match(filename, "bb$") then
+        recipetype = "recipe"
+    elseif string.match(filename, "bbappend$") then
+        recipetype = "append"
+    else
+        return nil
+    end
+
+    local recipe = {}
+    recipe.filename = filename
+    recipe.layer = layer
+
+    local name, version = parse_filename(filename)
+
+    if M.index[name] == nil then
+        M.index[name] = {}
+    end
+
+    if version == nil and recipetype == "recipe" then
+        version = "unversioned"
+    end
+
+    add_recipe(name, version, recipetype, recipe)
 end
 
 -- Iterate over every file in the layer directory with a bitbake extension and parse the package
